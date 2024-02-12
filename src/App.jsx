c0dd07c35f85ac58cdb0a3e7add8b769fcf2c88e
erayerm <ermemiseray@gmail.com>
2024-02-12 19:33:46 +0300

    Creating "Editor's Pick" Section at Home Page #3

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { ToastContainer } from 'react-toastify'
 import './App.css'
 import Header from './layouts/Header'
 import Footer from './layouts/Footer'
+import HomePage from './page/HomePage'
 
 function App() {
 
   return (
     <>
       <Header />
+      <HomePage />
       <Footer />
 
       <ToastContainer />
---
+++ src/page/HomePage.jsx
@@ -0,0 +1,31 @@
+export default function HomePage() {
+
+    return (
+        <div>
+            {/*Carousel Will be Added Later*/}
+            <section className="w-screen bg-[#FAFAFA]">
+                <div className="my-0 mx-auto max-w-[1050px] flex flex-col items-center">
+                    <h3 className="text-2xl leading-8 font-bold pt-20 pb-12">EDITOR`&apos;`S PICK</h3>
+                    <p className="pt-5">Problems trying to resolve the conflict between</p>
+                    <div className="flex flex-wrap gap-7 pb-20">
+                        <div className="w-[510px] h-[500px] flex items-end bg-[url('/img/main-section-1-man.png')] bg-cover">
+                            <p className="py-3 px-[65px] text-black bg-white">MEN</p>
+                        </div>
+                        <div className="w-[240px] h-[500px] flex items-end bg-[url('/img/main-section-1-women.png')] bg-cover">
+                            <p className="py-3 px-3 text-black bg-white">WOMEN</p>
+                        </div>
+                        <div className="w-[240px] flex flex-col justify-between">
+                            <div className="w-full h-[240px] flex items-end bg-[url('/img/main-section-1-accessories.png')] bg-cover">
+                                <p className="py-3 px-3 text-black bg-white">ACCESSORIES</p>
+                            </div>
+                            <div className="w-full h-[240px] flex items-end bg-[url('/img/main-section-1-kids.png')] bg-cover">
+                                <p className="py-3 px-3 text-black bg-white">KIDS</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
