fa21617c66620f473af4770736ea588c375ae9db
erayerm <ermemiseray@gmail.com>
2024-02-12 15:07:20 +0300

    Creating Footer Layout #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { ToastContainer } from 'react-toastify'
 import './App.css'
 import Header from './layouts/Header'
+import Footer from './layouts/Footer'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Footer />
 
       <ToastContainer />
     </>
---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,40 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+
+export default function Footer() {
+    return (
+        <div className="w-screen bg-[#2A7CC7] text-white h-[414px]">
+            <div className="max-w-[1050px] h-[338px] flex justify-between mx-auto my-0 pt-20">
+                <div className="w-[250px] ">
+                    <h3 className='footer-titles'>Get In Touch</h3>
+                    <p className='text-[#8EC2F2]'>The quick fox jumps over the lazy dog</p>
+                    <div className='flex gap-5 pt-5 text-[24px]'>
+                        <FontAwesomeIcon icon={faFacebook} />
+                        <FontAwesomeIcon icon={faInstagram} />
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </div>
+                </div>
+                <div className="w-[250px]">
+                    <h3 className='footer-titles'>Company info</h3>
+                    <ul className='flex flex-col gap-2 text-[#8EC2F2]'>
+                        <li>About Us</li>
+                        <li>Carrier</li>
+                        <li>We are hiring</li>
+                        <li>Blog</li>
+                    </ul>
+                </div>
+                <div className="w-[250px]">
+                    <h3 className='footer-titles'>Features</h3>
+                    <ul className='flex flex-col gap-2 text-[#8EC2F2]'>
+                        <li>Bussiness Marketing</li>
+                        <li>User Analytic</li>
+                        <li>Live Chat</li>
+                        <li>Unlimited Support</li>
+                    </ul>
+                </div>
+            </div>
+            <div className='w-full flex justify-center items-center h-[75px] text-sm leading-6 font-bold'>Made With Love By Figmaland All Right Reserved</div>
+        </div>
+    )
+}
