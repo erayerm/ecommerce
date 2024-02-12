5bd8f44d1e1e39cd6a67415aa9621d2da0b5f261
erayerm <ermemiseray@gmail.com>
2024-02-12 14:48:59 +0300

    Creating Header Layout #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { ToastContainer } from 'react-toastify'
 import './App.css'
+import Header from './layouts/Header'
 
 function App() {
 
   return (
     <>
-
-
+      <Header />
 
       <ToastContainer />
     </>
