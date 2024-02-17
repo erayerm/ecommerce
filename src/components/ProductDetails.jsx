5554d03131560c9279f753dc21c7c83a12767abd
erayerm <ermemiseray@gmail.com>
2024-02-18 00:52:35 +0300

    changing ProductPage's description section

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -2,46 +2,43 @@ import { Route, Switch, NavLink } from "react-router-dom/cjs/react-router-dom.mi
 import Description from "../layouts/Description";
 import AdditionalInformation from "../layouts/AdditionalInformation";
 import Reviews from "../layouts/Reviews";
+import { useState } from "react";
 
 
 export default function ProductDetails() {
+    const [activeTab, setActiveTab] = useState('description');
 
+    const handleTab = (event) => {
+        setActiveTab(event.target.id);
+    }
 
     return (
-        <section className="w-screen">
-            <div className="mx-auto max-w-page-content flex flex-col">
-                <div className="flex justify-center">
-                    <NavLink exact to="/productPage" className={isActive => "p-6 text-sm font-bold leading-6 " + (!isActive ? "text-gray" : "text-primary-blue")}>
-                        <NavLink to="/productPage/description/" className={isActive => "p-6 text-sm font-bold leading-6 " + (!isActive ? "" : "text-primary-blue")}>
-                            Description
-                        </NavLink>
-                    </NavLink>
-
-                    <NavLink to="/productPage/additionalInformation" className={isActive => "p-6 text-sm font-bold leading-6 " + (!isActive ? "text-gray" : "text-primary-blue")}>
+        <section className='w-screen'>
+            <div className="max-w-page-content mx-auto flex flex-col">
+                <div className='flex justify-center'>
+                    <a id="description" className={"product-tabs " + (activeTab === 'description' ? 'text-primary-blue font-bold' : '')} onClick={handleTab}>
+                        Description
+                    </a>
+                    <a id="additional" className={"product-tabs " + (activeTab === 'additional' ? 'text-primary-blue font-bold' : '')} onClick={handleTab}>
                         Additional Information
-                    </NavLink>
-                    <NavLink to="/productPage/reviews" className={isActive => "p-6 text-sm font-bold leading-6 " + (!isActive ? "text-gray" : "text-primary-blue")}>
-                        Reviews
-                    </NavLink>
+                    </a>
+                    <a id="reviews" className={"product-tabs " + (activeTab === 'reviews' ? 'text-primary-blue font-bold' : '')} onClick={handleTab}>
+                        Reviews <span className="font-primary-blue font-bold">(0)</span>
+                    </a>
                 </div>
                 <hr className="h-[2px] border-0 mb-5 bg-[#ECECEC]" />
-                <div className="pt-6 pb-10">
-                    <Switch>
-                        <Route exact path="/productPage">
-                            <Description />
-                        </Route>
-                        <Route path="/productPage/description">
-                            <Description />
-                        </Route>
-                        <Route path="/productPage/additionalInformation">
-                            <AdditionalInformation />
-                        </Route>
-                        <Route path="/productPage/reviews">
-                            <Reviews />
-                        </Route>
-                    </Switch>
+                <div className="pb-10">
+                    <div>
+                        {
+                            {
+                                'description': <Description />,
+                                'additional': <AdditionalInformation />,
+                                'reviews': <Reviews />
+                            }[activeTab]
+                        }
+                    </div>
                 </div>
             </div>
         </section>
-    )
+    );
 }
