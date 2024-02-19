2f12829e1b1a655b0739efae5b1c6e5ba1546a56
erayerm <ermemiseray@gmail.com>
2024-02-19 18:06:28 +0300

    Creating Contact Page's mobile view #8

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
     return (
         <>
             <section className="w-screen">
-                <div className="max-w-page-content mx-auto flex justify-between items-center">
+                <div className="max-w-page-content mx-auto flex lg:flex-col lg:text-center lg:gap-[180px] lg:pt-[140px] justify-between xl:justify-center items-center">
                     <div className="flex flex-col gap-[35px] max-w-[400px]">
                         <h4 className="font-bold leading-6 text-main">CONTACT US</h4>
                         <h1 className="font-bold text-6xl leading-20 text-main">Get in touch
@@ -17,15 +17,15 @@ export default function Contact() {
                             <p>Phone: +451 215 215 </p>
                             <p>Fax: +451 215 215</p>
                         </div>
-                        <div className='flex gap-[34px] text-[30px] text-main'>
+                        <div className='flex gap-[34px] text-[30px] text-main lg:justify-center'>
                             <FontAwesomeIcon icon="fa-brands fa-twitter" />
                             <FontAwesomeIcon icon="fa-brands fa-facebook" />
                             <FontAwesomeIcon icon="fa-brands fa-instagram" />
                             <FontAwesomeIcon icon="fa-brands fa-linkedin" />
                         </div>
                     </div>
-                    <div>
-                        <img src="./img/contact-hero.svg" className="min-w-[632px] min-h-[612px] mr-[-128px]" />
+                    <div className="lg:w-full lg:overflow-hidden">
+                        <img src="./img/contact-hero.svg" className="max-w-[632px] max-h-[612px] lg:w-full mr-[-128px] lg:mx-auto" />
                     </div>
                 </div>
             </section>
@@ -35,10 +35,10 @@ export default function Contact() {
                 <div className='max-w-page-content mx-auto flex flex-col items-center py-[112px] gap-20'>
                     <div className='text-main text-center flex flex-col gap-2.5 '>
                         <h3 className='text-sm leading-5'>VISIT OUR OFFICE</h3>
-                        <h2 className='font-bold text-4.5xl leading-[50px] max-w-[555px]'>We help small businesses
+                        <h2 className='font-bold text-4.5xl leading-12.5 max-w-[555px]'>We help small businesses
                             with big ideas</h2>
                     </div>
-                    <div className='flex'>
+                    <div className='flex flex-wrap justify-center'>
                         {contactCardData.map((item, index) => {
                             return <ContactCard key={index} data={item} index={index} />
                         })}
@@ -52,7 +52,7 @@ export default function Contact() {
                     <h3 className='leading-6'>WE Can&apos;t WAIT TO MEET YOU</h3>
                     <h2 className='text-6xl leading-20'>Let’s Talk</h2>
                     <div>
-                        <button className='text-white text-sm leading-[22px] bg-primary-blue rounded px-10 py-4'>Try it free now</button>
+                        <button className='text-white text-sm leading-5.5 bg-primary-blue rounded px-10 py-4'>Try it free now</button>
                     </div>
                 </div>
             </section>
