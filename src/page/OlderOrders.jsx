a5d85738c70a4ba47fa050ce30f3e976f695de6c
erayerm <ermemiseray@gmail.com>
2024-03-13 11:53:03 +0300

    Older Orders Page fix #20

--- src/page/OlderOrders.jsx
+++ src/page/OlderOrders.jsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 import { ecommerceAPI } from "../instance";
 import { useSelector } from "react-redux";
 
-const dummyAllProducts = [322, 323, 324, 325, 326, 327];
-const dummyOrderProducts = [322, 323, 324];
 export default function OlderOrders() {
     const token = useSelector(store => store.user.user.token)
     const [orders, setOrders] = useState([]);
@@ -26,27 +24,15 @@ export default function OlderOrders() {
     }, [])
 
     useEffect(() => {
-        //FIXME: Pending backend fix
-        /*
         orders.map((ord) =>
             ord.products.map(prod => {
                 ecommerceAPI.get("/products/" + prod.id, {
                     headers: {
                         Authorization: token
                     }
-                }).then(res => setAllOrderedProducts([...allOrderedProducts, res.data]))
-            })
-        )
-        */
-        if (orders.length !== 0) {
-            dummyAllProducts.map(item => {
-                ecommerceAPI.get("/products/" + item, {
-                    headers: {
-                        Authorization: token
-                    }
                 }).then(res => setAllOrderedProducts((prev) => [...prev, res.data]))
             })
-        }
+        )
     }, [orders]);
 
     const findAddress = (index) => {
@@ -57,13 +43,12 @@ export default function OlderOrders() {
         <div className="max-w-page-content lg:px-2 mx-auto pt-5">
             <div className="w-100">
                 <div className="flex gap-[4%] align-items-center">
-                    <div className={"flex w-[48%] min-w-[200px] aspect-[1.5/1]" + (dummyOrderProducts.length >= 3 ? " space-x-[-10%]" : "") + (dummyOrderProducts.length === 2 ? " space-x-[-20%]" : "")}>
-                        {//FIXME: Pending backend fix
-                            dummyOrderProducts.map((item, index) => {
-                                if (index > 2) return "";
-                                const imageURL = allOrderedProducts[allOrderedProducts.findIndex((i) => i.id == item)]?.images[0].url
-                                return <img className={"border object-cover object-center" + (dummyOrderProducts.length >= 3 ? " w-[40%]" : "") + (dummyOrderProducts.length === 2 ? " w-[60%]" : "") + (dummyOrderProducts.length === 1 ? " w-full" : "")} src={imageURL} key={index} />
-                            })}
+                    <div className={"flex w-[48%] min-w-[200px] aspect-[1.5/1]" + (orders[0]?.products.length >= 3 ? " space-x-[-10%]" : "") + (orders[0]?.products.length === 2 ? " space-x-[-20%]" : "")}>
+                        {orders[0]?.products.map((item, index) => {
+                            if (index > 2) return "";
+                            const imageURL = allOrderedProducts[allOrderedProducts.findIndex((i) => i.id == item.id)]?.images[0].url
+                            return <img className={"border object-cover object-center" + (orders[0]?.products.length >= 3 ? " w-[40%]" : "") + (orders[0]?.products.length === 2 ? " w-[60%]" : "") + (orders[0]?.products.length === 1 ? " w-full" : "")} src={imageURL} key={index} />
+                        })}
                     </div>
                     <div className="text-xl max-w-[48%] md:text-sm">
                         <h1 className="text-3xl font-bold pb-5 md:text-md">Siparişin Tamamlandı!</h1>
@@ -77,13 +62,12 @@ export default function OlderOrders() {
             <div className="flex flex-wrap gap-x-[6%] gap-y-3 lg:flex-col md:text-sm w-full">
                 {orders?.slice(1).map((ord, ordIndex) => <div className="basis-[47%]" key={ordIndex}>
                     <div className="flex gap-[2%] w-full h-full items-center">
-                        <div className={"flex w-[38%] min-w-[200px] aspect-[1.5/1]" + (dummyOrderProducts.length >= 3 ? " space-x-[-10%]" : "") + (dummyOrderProducts.length === 2 ? " space-x-[-20%]" : "")}>
-                            {//FIXME: Pending backend fix
-                                dummyOrderProducts.map((item, index) => {
-                                    if (index > 2) return "";
-                                    const imageURL = allOrderedProducts[allOrderedProducts.findIndex((i) => i.id == item)]?.images[0].url
-                                    return <img className={"border object-cover object-center" + (dummyOrderProducts.length >= 3 ? " w-[40%]" : "") + (dummyOrderProducts.length === 2 ? " w-[60%]" : "") + (dummyOrderProducts.length === 1 ? " w-full" : "")} src={imageURL} key={index} />
-                                })}
+                        <div className={"flex w-[38%] min-w-[200px] aspect-[1.5/1]" + (ord.products.length >= 3 ? " space-x-[-10%]" : "") + (ord.products.length === 2 ? " space-x-[-20%]" : "")}>
+                            {ord.products.map((item, index) => {
+                                if (index > 2) return "";
+                                const imageURL = allOrderedProducts[allOrderedProducts.findIndex((i) => i.id == item.id)]?.images[0].url
+                                return <img className={"border object-cover object-center" + (ord.products.length >= 3 ? " w-[40%]" : "") + (ord.products.length === 2 ? " w-[60%]" : "") + (ord.products.length === 1 ? " w-full" : "")} src={imageURL} key={index} />
+                            })}
                         </div>
                         <div className="max-w-[60%]">
                             <p><span className="font-bold">Sipariş Tarihi:</span> {orders[ordIndex]?.order_date.slice(0, 10)} / {orders[ordIndex]?.order_date.slice(11, 16)}</p>
